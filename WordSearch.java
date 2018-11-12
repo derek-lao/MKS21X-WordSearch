5e3ab784dbf38f2e3e505088025f4d08b8d95c09
Derek Lao <dlao00@stuy.edu>
2018-11-12 16:36:16 -0500

    fixed all methods as of 11/8/18, and added the stuff from 11/9/18 about seed and randgen, as well as importing the random, scanner, and arraylist

--- WordSearch.java
+++ WordSearch.java
@@ -1,5 +1,19 @@
+import java.util.*; //random, scanner, arraylist
+import java.io.*; //file, filenotfoundexception
 public class WordSearch{
     private char[][] data;
+
+//the random seed used to produce this WordSearch
+    private int seed;
+
+    //a random Object to unify your random calls
+    private Random randgen;
+
+    //all words from a text file get added to wordsToAdd, indicating that they have not yet been added
+    private ArrayList<String>wordsToAdd;
+
+    //all words that were successfully added get moved into wordsAdded.
+    private ArrayList<String>wordsAdded;
 
     /**Initialize the grid to the size specified
      *and fill all of the positions with '_'
@@ -8,13 +22,7 @@ public class WordSearch{
      */
     public WordSearch(int rows,int cols){
       data=new char[rows][cols];
-      for(int r=0;r<data.length;r++)
-      {
-        for(int c=0;c<data[r].length;c++)
-        {
-          data[r][c]='_';
-        }
-      }
+      clear();
     }
 
     /**Set all values in the WordSearch to underscores'_'*/
@@ -74,15 +82,12 @@ public class WordSearch{
     public boolean addWordHorizontal(String word,int row, int col){
       for(int i=0;i<word.length();i++)
       {
-        if(!(data[row][col+i]=='_' && row<data.length && col<data[row].length))
+        if(!(row<data.length && col+i<data[row].length && (data[row][col+i]=='_' || data[row][col+i]==word.charAt(i))))
         return false;
       }
       for(int i=0;i<word.length();i++)
       {
-        if(data[row][col+i]=='_' && row<data.length && col<data[row].length)
-        {
-          data[row][col+i]=word.charAt(i);
-        }
+        data[row][col+i]=word.charAt(i);
       }
       return true;
     }
@@ -101,16 +106,37 @@ public class WordSearch{
     public boolean addWordVertical(String word,int row, int col){
       for(int i=0;i<word.length();i++)
       {
-        if(!(data[row+i][col]=='_' && row<data.length && col<data[row].length))
+        if(!(row+i<data.length && col<data[row].length && (data[row+i][col]=='_' || data[row+i][col]==word.charAt(i))))
         return false;
       }
       for(int i=0;i<word.length();i++)
       {
-        if(data[row+i][col]=='_' && row<data.length && col<data[row].length)
-        {
-          data[row+i][col]=word.charAt(i);
-        }
+        data[row+i][col]=word.charAt(i);
       }
       return true;
     }
+
+    /**Attempts to add a given word to the specified position of the WordGrid.
+     *The word is added from top left to bottom right, must fit on the WordGrid,
+     *and must have a corresponding letter to match any letters that it overlaps.
+     *
+     *@param word is any text to be added to the word grid.
+     *@param row is the vertical locaiton of where you want the word to start.
+     *@param col is the horizontal location of where you want the word to start.
+     *@return true when the word is added successfully. When the word doesn't fit,
+     *or there are overlapping letters that do not match, then false is returned.
+     */
+    public boolean addWordDiagonal(String word,int row, int col){
+      for(int i=0;i<word.length();i++)
+      {
+        if(!(row+i<data.length && col+i<data[row].length && (data[row+i][col+i]=='_' || data[row+i][col+i]==word.charAt(i))))
+        return false;
+      }
+      for(int i=0;i<word.length();i++)
+      {
+        data[row+i][col+i]=word.charAt(i);
+      }
+      return true;
+    }
+
 }
